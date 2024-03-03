241bbfd16dc5da64a7b7dc07fd3787f25199d61b
DandSnow <danilapasenko3@gmail.com>
2024-03-03 14:03:29 +0600

    camera

--- core/src/com/mygdx/game/MyHorrorGame.java
+++ core/src/com/mygdx/game/MyHorrorGame.java
@@ -1,18 +1,29 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Vector;
+
 public class MyHorrorGame extends ApplicationAdapter {
+	public static final float SCR_WIDTH = 900, SCR_HEIGHT = 1600;
+
 	SpriteBatch batch;
 	Texture img;
+	Vector3 touch;
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera =  new OrthographicCamera();
+		camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+		touch = new Vector3();
 	}
 
 	@Override

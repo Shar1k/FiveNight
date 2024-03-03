87056aef4fdd48bcf3b71f4d0548ba35b5859ff2
DandSnow <danilapasenko3@gmail.com>
2024-03-03 13:46:16 +0600

    ffjfj

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("FiveFight");
+		config.setTitle("FiveMight");
 		new Lwjgl3Application(new MyHorrorGame(), config);
 	}
 }
